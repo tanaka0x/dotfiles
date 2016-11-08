a9ca26df1dd94b36b514e512022952c3a970abb7
tanaka0x <tanaka@tanaka-air.local>
2016-11-08 11:08:05 +0900

    empty .emacs.d

--- .emacs.d/elpa/hc-zenburn-theme-20141031.355/hc-zenburn-theme-autoloads.el
+++
@@ -1,26 +0,0 @@
-;;; hc-zenburn-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil "hc-zenburn-theme" "hc-zenburn-theme.el" (21613
-;;;;;;  33767 692373 268000))
-;;; Generated autoloads from hc-zenburn-theme.el
-
-(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;;;### (autoloads nil nil ("hc-zenburn-theme-pkg.el") (21613 33767
-;;;;;;  706425 200000))
-
-;;;***
-
-(provide 'hc-zenburn-theme-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; hc-zenburn-theme-autoloads.el ends here
--- .emacs.d/elpa/rubocop-20150630.1003/rubocop-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "rubocop" "20150630.1003" "An Emacs interface for RuboCop" (quote ((dash "1.0.0") (emacs "24"))))
